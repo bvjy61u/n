260cd2f8c45e8bf33e9822ddb0512ce1b80487b8
Israel Carbajal <bvjy61u@icloud.com>
2022-05-28 23:33:47 -0600

    Simple Change Commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Sidebar  from './SideBar';
+import Main from './Main';
 
 function App() {
+  const[notes,setNotes] = useState([]);
+  const[activeNote,setActiveNote] = useState(false);
+
+  const onAddNote = () => {
+    
+    const newNote = {
+      id: Date.now() ,
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote,...notes]);
+  };
+
+  const onUpdateNote = (updatedNote) =>{
+    const updatedNotesArray = notes.map((note) =>{
+      if(note.id ===activeNote){
+        return updatedNote;
+      }
+      return note;
+    })
+    setNotes(updatedNotesArray)
+  };
+
+  const onDeleteNote = (idToDelete) =>{
+    setNotes(notes.filter((note)=>note.id !==idToDelete));
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id ===activeNote);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          This is a deployed page.
-        </p>
-        <p>
-          2022
-        </p>
-        <p>
-          copyright 2023
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <Sidebar 
+    notes={notes} 
+    onAddNote={onAddNote}
+    onDeleteNote={onDeleteNote}
+    activeNote = {activeNote}
+    setActiveNote = {setActiveNote}
+    />
+
+    <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
+
     </div>
   );
 }
